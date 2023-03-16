8107dc3204284d37482bca331b70ef919848b56e
Branden Jones <branden.wayne.jones@gmail.com>
2023-03-16 16:04:54 -0400

    NavBar showing, missing components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,30 @@
-import React from "react";
-import { Avatar, AppBar } from "@mui/material";
+import React, { useState } from "react";
+import { Avatar, AppBar, Box, Toolbar, IconButton, Typography, Button, MenuItem } from "@mui/material";
+
+// const 
 
 const  NavBar = () => {
-    console.log(`Hello from NavBar`)
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                <IconButton
+                    size="large"
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                    sx={{ mr: 2 }}
+                >
+                    <MenuItem />
+                </IconButton>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    News
+                </Typography>
+                <Button color="inherit">Contact</Button>
+                </Toolbar>
+            </AppBar>
+            </Box>
+    )
 }
 
 export default NavBar;
